a410d0a0356d0db059068d98a0a6c6404c115766
lmocs <loganmoreno.cs@gmail.com>
2025-04-09 13:03:10 -0700

    Create service for the `users` table.

---
+++ server/services/userService.js
@@ -0,0 +1,31 @@
+const db = require('../config/db');
+
+const User = {
+	getUsers: async () => {
+		const sql = `SELECT * FROM users`;
+
+		try {
+			const results = await db.query(sql);
+			console.log('All users: ', results);
+			return results;
+		} catch (err) {
+			console.error('Error in getUsers:', err);
+			throw err;
+		}
+	},
+
+	addUser: async (userData) => {
+		const sql = `INSERT INTO users (wallet_address, username) VALUES (?, ?)`;
+		const values = [userData.wallet_address, userData.username];
+
+		try {
+			const results = await db.query(sql, values);
+			return results;
+		} catch (err) {
+			console.error('Error in addUser:', err);
+			throw err;
+		}
+	},
+};
+
+module.exports = User;
