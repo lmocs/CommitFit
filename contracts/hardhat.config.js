17a53de4a72e38d6f6ed0c13923793f277c9e3b6
Byron Tran <159394559+byrontran@users.noreply.github.com>
2025-05-07 00:38:43 -0700

    Containerized hardhat node and PactFactory deployment

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -2,5 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    localhost: {
+      url: "http://hardhat-node:8545",
+    },
+  },
   solidity: "0.8.28",
 };
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -4,11 +4,11 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contracts with:", deployer.address);
 
-  const PactFactory = await.ethers.getContractFactory("PactFactory");
+  const PactFactory = await ethers.getContractFactory("PactFactory");
   const pactFactory = await PactFactory.deploy();
-  await pactFactory.deployed();
+  await pactFactory.waitForDeployment();
 
-  console.log("PactFactory deployed to:", pactFactory.address);
+  console.log("PactFactory deployed to:", await pactFactory.getAddress());
 }
 
 main()
